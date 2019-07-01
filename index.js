fd38f4b69f6ba3c6e464a83bbc094bd2a4acfcda
root <root@Xing.localdomain>
2019-07-01 11:30:49 -0700

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 8001
 const app = express();
 const { Pool } = require('pg');
 var bodyParser = require('body-parser');
