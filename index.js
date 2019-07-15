790b7d3ec4770b898a99d53b94d1fd58d0fc0a6d
leo qiu <qiuweiq@sfu.ca>
2019-07-15 15:01:10 -0700

    api test

--- index.js
+++ index.js
@@ -45,7 +45,12 @@ app.post('/poke',function(req,res){
   var name='eevee';
   P.getPokemonByName('eevee',function(response,error){
     if(!error){
-      console.log(response);
+      //console.log(response);
+      console.log(response.ability[0]);
+      console.log(response.stats[0]); //0-5
+      console.log(response.type[0]);
+      console.log(response.height);
+      console.log(response.weight);
     } 
     else {
       console.log(error);
