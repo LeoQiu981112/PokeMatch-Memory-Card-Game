d9ed3f53e682a75d3842e3bd81a769cefa343c89
leo qiu <qiuweiq@sfu.ca>
2019-07-16 16:03:02 -0700

    current working version 1

--- index.js
+++ index.js
@@ -211,8 +211,8 @@ app.get('/userlist',function(req,res){
 app.get('/logout',function(req,res){
   console.log("server receive logout req");
   console.log("destroying session");
+  req.session.isLogin= false;
   req.session.destroy();
-  //req.session.isLogin= false;
   res.json({status:-1,msg:"session ended"});
 });
 
