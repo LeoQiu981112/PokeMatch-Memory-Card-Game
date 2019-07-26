eb944edd91cad953cc05964d758d7f4cca2120e0
qiuweiq <qiuweiq@sfu.ca>
2019-07-26 16:56:46 -0700

    chat1

--- public/tmp.js
+++ public/tmp.js
@@ -1,26 +1,44 @@
 $(function() {
   var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
   var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
   ];
 
+
+  // get session name with ajax,
+  $.ajax({
+      type:"get",
+      url:"/userlist",
+      success:function(data){
+          if(data.status==-1){
+              var user="";
+              user+=data.user;
+          }
+         // var html="";
+         // html+=data.status;
+          //$("tbody").html(html);
+      },
+      error:function(){
+          alert("Internet Error");
+      }
+  });   
+
+
   // Initialize variables
   var $window = $(window);
-  var $usernameInput = $('.usernameInput'); // Input for username
+
+
   var $messages = $('.messages'); // Messages area
   var $inputMessage = $('.inputMessage'); // Input message input box
 
-  var $loginPage = $('.login.page'); // The login page
   var $chatPage = $('.chat.page'); // The chatroom page
 
   // Prompt for setting a username
   var username;
   var connected = false;
-  var typing = false;
-  var $currentInput = $usernameInput.focus();
+  var $currentInput = $inputMessage.focus();
 
   var socket = io();
 
@@ -36,21 +54,14 @@ $(function() {
 
   // Sets the client's username
   const setUsername = () => {
-    username = cleanInput($usernameInput.val().trim());
     // If the username is valid
     if (username) {
-      $loginPage.fadeOut();
       $chatPage.show();
-      $loginPage.off('click');
       $currentInput = $inputMessage.focus();
-
       // Tell the server your username
-      socket.emit('add user', username);
+      socket.emit('add user', user);
     }
   }
-
-  
-
 
   // Sends a chat message
   const sendMessage = () => {
@@ -183,7 +194,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to the Chat";
     log(message, {
       prepend: true
     });
@@ -213,15 +224,5 @@ $(function() {
     log('you have been disconnected');
   });
 
-  socket.on('reconnect', () => {
-    log('you have been reconnected');
-    if (username) {
-      socket.emit('add user', username);
-    }
-  });
-
-  socket.on('reconnect_error', () => {
-    log('attempt to reconnect has failed');
-  });
 
 });
