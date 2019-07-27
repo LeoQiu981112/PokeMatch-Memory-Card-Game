217533c7c46d9505f48a03eb345e73d029347651
qiuweiq <qiuweiq@sfu.ca>
2019-07-27 12:28:46 -0700

    chat test1.16 name test

--- public/tmp.js
+++ public/tmp.js
@@ -41,7 +41,7 @@ $(function() {
   var connected = false;
   // var typing = false;
   // var lastTypingTime;
-  // var $currentInput = $usernameInput.focus();
+  var $currentInput = $inputMessage.focus();
 
   var socket = io();
 
