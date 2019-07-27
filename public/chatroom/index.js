c5ee3050891fff143d240b2d1d61d42692766e43
TaoJing <Gin@TaoJings-MacBook-Air.local>
2019-07-26 18:29:29 -0700

    create a simple chatroom page on localhost:3000

---
+++ public/chatroom/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/chatroom.html');
+});
+
+io.sockets.on('connection', function(socket){
+	socket.on('send message', function(mes){
+		io.sockets.emit('new message', mes);
+		// socket.broadcast.emit('new message', mes);
+	});
+});
+
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
