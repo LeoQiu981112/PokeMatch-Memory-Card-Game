b057f732b6070b9f4970ce9e0a2fb2353cdfc058
root <root@Xing.localdomain>
2019-07-28 13:46:09 -0700

    test

--- public/js/script.js
+++ public/js/script.js
@@ -8,17 +8,18 @@ socket.on('waiting', function(str) {
 socket.on('ready', function(str) {
     document.getElementById('ready').innerHTML = str;
 })
-socket.on('5togo', function(){
-    var i = 5; 
-    $("#5togoshow").show(); 
-    var intervalid; 
-    intervalid = setInterval("fun()", 1000); 
-    function fun() { 
-        if (i == 0) { 
-            $("#startpage").hide();
-            clearInterval(intervalid); 
-        }    
-        document.getElementById("mes").innerHTML = i; 
-        i--; 
-    }       
-})
+
+//socket.on('5togo', function(){
+//    var i = 5; 
+//    $("#5togoshow").show(); 
+///    var intervalid; 
+//    intervalid = setInterval("fun()", 1000); 
+//    function fun() { 
+//        if (i == 0) { 
+//            $("#startpage").hide();
+//            clearInterval(intervalid); 
+//        }    
+//        document.getElementById("mes").innerHTML = i; 
+//        i--; 
+//    }       
+//})
