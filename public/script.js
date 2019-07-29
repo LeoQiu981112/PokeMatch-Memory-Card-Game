1bb356ca9bf68bdd8a461353052a4b5b43a192c8
root <root@Xing.localdomain>
2019-07-28 19:11:46 -0700

    test

--- public/script.js
+++ public/script.js
@@ -1,1 +1,24 @@
+var socket = io();
+var local = new Local(socket);
+var remote = new Remote(socket);
+
+socket.on('waiting', function(str) {
+    document.getElementById('waiting').innerHTML = str;
+})
+socket.on('ready', function(str) {
+    document.getElementById('ready').innerHTML = str;
+    $("#3togoshow").show();
+    var i = 3;      
+    var intervalid; 
+    function fun() { 
+        if (i == 0) { 
+            $("#startpage").hide();
+            clearInterval(intervalid); 
+        }    
+        document.getElementById("mes").innerHTML = i; 
+        i--; 
+    } 
+    intervalid = setInterval(fun, 1000); 
+    
+})
 
