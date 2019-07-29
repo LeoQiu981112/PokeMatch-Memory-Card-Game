a543ef0a335e611332cb504875cd9bb6d2dadd99
root <root@Xing.localdomain>
2019-07-28 21:58:17 -0700

    test

--- public/js/remote.js
+++ public/js/remote.js
@@ -5,6 +5,7 @@ var Remote = function(socket){
     // 绑定按钮事件
     var bindEvents = function() {
         socket.on('init', function(data){
+            console.log(data);
             start(data.type, data.dir);
         })
 
